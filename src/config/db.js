b107c9bff9a751ce79ffec0b65ce730f297e235e
mahmoudmansour <mahmoudmansooour@gmail.com>
2026-04-01 03:46:34 +0200

    new

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const dbConnection = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/blog-app-new");
+    await mongoose.connect(
+      "mongodb+srv://mahmoudmansooour_db_user:Gx271Hbg85SADfJd@cluster0.ktkwn7s.mongodb.net/?appName=Cluster0",
+    );
     console.log("DB Connectd Successfuly");
   } catch (error) {
     console.log(error);
