188f7e351ee7d47defc2bf0dda17a0d068addcef
mahmoudmansour <mahmoudmansooour@gmail.com>
2026-04-01 04:14:12 +0200

    S

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://gravo-design.online/",
+    origin: "https://gravo-design.online",
     credentials: true,
   }),
 );
