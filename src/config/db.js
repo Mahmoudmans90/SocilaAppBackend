c54f01822ac521be3201aa6360984f28255d8961
mahmoudmansour <mahmoudmansooour@gmail.com>
2026-04-01 04:36:04 +0200

    ssss

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://mahmoudmansooour_db_user:Gx271Hbg85SADfJd@cluster0.ktkwn7s.mongodb.net/?appName=Cluster0",
+      "mongodb+srv://mahmoudmansooour_db_user:Gx271Hbg85SADfJd@cluster0.ktkwn7s.mongodb.net/social-app?retryWrites=true&w=majority",
     );
     console.log("DB Connectd Successfuly");
   } catch (error) {
